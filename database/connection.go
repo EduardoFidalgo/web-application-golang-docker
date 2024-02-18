1aa95b05430b3534829633913b69a2bf7dabc8ea
Eduardo Fidalgo <eduardo.fidalgo@letmein.com.br>
2024-02-18 16:16:37 -0300

    creating a .env to save all sensitive variables

--- database/connection.go
+++ database/connection.go
@@ -3,19 +3,37 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func Connect() *sql.DB {
-	db, err := sql.Open("mysql", "root:secret@tcp(mysql:3306)/webservice-database")
-	if err != nil {
-		panic(err.Error())
+	// Carregar variáveis de ambiente do arquivo .env
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Erro ao carregar arquivo .env: %v", err)
 	}
 
-	err = db.Ping()
+	// Construir string de conexão com base nas variáveis de ambiente
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	// Conectar ao banco de dados
+	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
+	}
+
+	// Verificar a conexão
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Erro ao verificar conexão com o banco de dados: %v", err)
 	}
 
 	fmt.Println("HeartBeat: MYSQL connected succefully")
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/webservice-golang-api
 
 go 1.21.3
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
